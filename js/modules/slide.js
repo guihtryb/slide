63299a966d7521f01ed717023f6635c2ee64a154
Guilherme Viana <guilhermehviana01@gmail.com>
2022-07-10 19:47:22 -0300

    feature: calc distance to slide move

--- js/modules/slide.js
+++ js/modules/slide.js
@@ -2,17 +2,39 @@ export default class Slide {
   constructor(_slide, _wrapper) {
     this.slide = document.querySelector(_slide);
     this.wrapper = document.querySelector(_wrapper);
+    this.distances = {
+      finalPosition: 0,
+      startX: 0,
+      movement: 0,
+    }
   }
 
-  onMove(event) { }
+  moveSlide(distX) {
+    this.distances.movedPosition = distX;
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`
+  }
+
+  updatePosition(clientX) {
+    this.distances.movement = (this.distances.startX - clientX) * 1.6;
+    return this.distances.finalPosition - this.distances.movement;
+  }
+
+  onMove(event) {
+    const finalPosition = this.updatePosition(event.clientX);
+    this.moveSlide(finalPosition);
+  }
 
   onStart(event) {
     event.preventDefault();
+
+    this.distances.startX = event.clientX;
+
     this.wrapper.addEventListener('mousemove', this.onMove);
   }
 
   onEnd(event) {
-    this.wrapper.removeEventListener('mousemove', this.onMove)
+    this.wrapper.removeEventListener('mousemove', this.onMove);
+    this.distances.finalPosition = this.distances.movedPosition;
   }
 
   addSlideEvent() {
@@ -22,9 +44,10 @@ export default class Slide {
 
   bindEvents() {
     this.addSlideEvent = this.addSlideEvent.bind(this);
+    this.updatePosition = this.updatePosition.bind(this);
     this.onStart = this.onStart.bind(this);
     this.onEnd = this.onEnd.bind(this);
-    this.onMove = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
   }
 
   init() {
